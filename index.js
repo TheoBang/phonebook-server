b20bcbf7050e592845257edd54fb46331a555c6a
Theodore Bang <theo.s.bang@gmail.com>
2025-04-13 21:14:18 -0500

    Finish step 8: custom Morgan token

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('content', function(req, res, param) {
+    return JSON.stringify(req.body)
+});
+app.use(morgan(':method :status :res[content-length] - :response-time ms :content'));
+
 
 let phonebook = [
     { 
